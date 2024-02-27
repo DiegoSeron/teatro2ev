867cfcd18d15435cd5bb7d134a9599a53560a612
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-27 19:54:58 +0100

    created class obraCreateDTO and obraUpdateDTO

---
+++ NET/Models/ObraUpdateDTO.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Tickett.Models;
+using System.Text;
+public class ObraUpdateDTO
+{
+     [Required]
+    public string Titulo { get; set; }
+    
+     [Required]
+    public string Descripcion { get; set; }
+     [Required]
+    public int Precio { get; set; }
+     [Required]
+    public DateTime DiaObra { get; set; }
+
+}
