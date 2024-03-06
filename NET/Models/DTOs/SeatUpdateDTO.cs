1105ffb7e801a7a3a5aa5352cf1c762dfa207a63
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-03-06 21:01:03 +0100

    Create Obra and Seat DTO

---
+++ NET/Models/DTOs/SeatUpdateDTO.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Tickett.Models;
+using System.Text;
+public class SeatUpdateDTO
+{
+    public bool Libre { get; set; }
+
+}
