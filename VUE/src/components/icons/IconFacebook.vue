17e28bb9ab71497c487c7dc3e16644163b57360b
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-03-11 17:35:13 +0100

    finished backend (all DTOs done)

--- VUE/src/components/icons/IconFacebook.vue
+++
@@ -1,3 +0,0 @@
-<template>
-        <img src="../../assets/iconos/icons8-facebook-100-blanco.png" alt="">
-</template>
--- VUE/src/components/icons/IconX.vue
+++
@@ -1,3 +0,0 @@
-<template>
-        <img src="../../assets/iconos/icons8-twitterx-100.png" alt="">
-</template>
